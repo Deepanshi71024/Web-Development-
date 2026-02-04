e677b1bba68771c1a47319cc62f138935904fdee
Deepanshi <deepanshi71024@gmail.com>
2026-02-04 11:08:06 +0530

    Removed unused files

--- script3.js
+++
@@ -1,6 +0,0 @@
-const text = document.getElementById("txt");
-const counter = document.getElementById("counter");
-
-text.addEventListener("input", function () {
-  counter.textContent = text.value.length;
-});
